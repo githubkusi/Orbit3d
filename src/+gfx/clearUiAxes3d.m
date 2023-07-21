9913ead31b0e8a98ebf258900fad139ffaca5f22
Markus Leuthold <markus.leuthold@sonova.com>
2023-07-21 16:12:20 +0200

    delete obsolete callbacks

--- src/+gfx/clearUiAxes3d.m
+++ src/+gfx/clearUiAxes3d.m
@@ -1,8 +1,13 @@
-function clearUiAxes3d(hParent)
+function hAxes = clearUiAxes3d(hParent)
 arguments
     hParent = gfx.getCurrentOrCreateUiFigure % normally figure or axis
 end
 hAxes = gfx.getCurrentOrCreateUiAxes(hParent);
+
+% Delete old callbacks, to avoid double callbacks. They are recreated with gfx.orbit3d
+if isfield(hAxes.UserData, 'UiEventList')
+    hAxes.UserData = rmfield(hAxes.UserData, 'UiEventList');
+end
 
 % cla kills the light, therefore the orbit is newly created
 cla(hAxes);
