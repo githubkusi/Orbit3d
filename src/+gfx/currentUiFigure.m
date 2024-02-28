c97b0fef09f3919dd8a7d1da48620bcfcaeb5d9f
Markus Leuthold <markus.leuthold@sonova.com>
2024-02-28 16:03:20 +0100

    do not touch java based figures
    - distinguish between new web and old java figures
    - remove browser identification via tag (not needed anymore)

--- src/+gfx/currentUiFigure.m
+++ src/+gfx/currentUiFigure.m
@@ -1,10 +1,13 @@
-function hFig = currentUiFigure
+function hUiFig = currentUiFigure
+% Return the current uifigure or create a new one if no uifigure exists.
+% Does not affect the old java figures
 hRoot = groot;
-hFigs = findobj(hRoot.Children, 'type', 'figure', '-not', 'tag', 'browser');
+hFigs = findobj(hRoot.Children, 'type', 'figure');
+hUiFigs = hFigs(arrayfun(@matlab.ui.internal.isUIFigure, hFigs));
 
-if isempty(hFigs)
-    hFig = uifigure("HandleVisibility", "on");
+if isempty(hUiFigs)
+    hUiFig = uifigure("HandleVisibility", "on");
 else
     % the first figure in the array is the current one
-    hFig = hFigs(1);
+    hUiFig = hUiFigs(1);
 end
