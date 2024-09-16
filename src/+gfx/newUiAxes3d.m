636b9d08364931c45addbe2d513f3ba44659219e
Markus Leuthold <markus.leuthold@sonova.com>
2024-09-16 22:37:16 +0200

    use full space
    uiaxes could probably be replaced by axes

--- src/+gfx/newUiAxes3d.m
+++ src/+gfx/newUiAxes3d.m
@@ -2,5 +2,7 @@ function hAxes = newUiAxes3d(hParent)
 arguments
     hParent = uifigure("HandleVisibility", "on");
 end
-hAxes = uiaxes(hParent);
+hAxes = uiaxes(hParent, ...
+    Units="normalized",...
+    Position=[0.1300 0.1100 0.7750 0.8150]);
 gfx.clearUiAxes3d(hAxes);
