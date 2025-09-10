4cd22f4da3b159827ae33e5864e85b93bcb09da6
Markus Leuthold <markus.leuthold@sonova.com>
2025-09-10 10:26:48 +0200

    update documentation

--- src/+gfx/uibrowser.m
+++ src/+gfx/uibrowser.m
@@ -3,23 +3,29 @@ function hBrowser = uibrowser(hFigure)
 %  hBrowserFigure = gfx.uibrowser
 %  hBrowserFigure = gfx.uibrowser(hFigure)
 %
-%  gfx.uibrowser intends to be a replacement for Matlab's plotbrowser()
-%  and also workswith new web based uifigures (in contrast to plotbrowser,
-%  which only supports the old java-based figures)
+%  gfx.uibrowser intends to be a replacement for Matlab's "plotbrowser"
+%  and also works with new web based "uifigure" (in contrast to plotbrowser,
+%  which only supports the old java-based "figure")
+%
+%  USAGE
+%    Press "b" in a figure to launch uibrowser
 %
 %  FEATURES
 %  - One browser figure per object figure
-%  - Supports objects of type patch/line/hggroup
+%  - Supports objects of type patch/line/hggroup/text
 %  - Supports multiple axes in one figure
 %  - Visibility on/off
 %  - Color copied from object, however object style is not yet reflected in
 %    the browser window
 %  - Close browser window with key "q"
-%  - Items are named according <graphichandle>.DisplayName. If DisplayName
-%    is empty, the object doesn't appear in the browser by default.
+%  - Items are named according <graphichandle>.DisplayName.
+%  - A hggroup is represented as corresponding tree in a uitree object
+%
+%  EXAMPLE
+%    Run "example_uibrowser"
 %
 %  AUTHOR
-%    Copyright 2022-2023, Markus Leuthold, markus.leuthold@sonova.com
+%    Copyright 2022-2025, Markus Leuthold, markus.leuthold@sonova.com
 %
 %  LICENSE
 %    BSD-3-Clause (https://opensource.org/licenses/BSD-3-Clause)
